f44016c3771fabee50f10fb8b9137aa643086e5a
MonkeSandwich <milosstrugger@gmail.com>
2025-12-10 12:21:46 -0500

    boss weapons

--- Assets/Scripts/Weapons/ProjectileWeapon.cs
+++ Assets/Scripts/Weapons/ProjectileWeapon.cs
@@ -20,62 +20,69 @@ public class ProjectileWeapon : Weapon
         {
             for (int i = 0; i < stats.amount; i++)
             {
+                float baseAngle = 90f;
+
                 switch (weaponData.projectileFiringType)
                 {
                     case ProjectileFiringType.None:
-                        firingAngle = 90f;
+                        baseAngle = 90f;
                         break;
                     case ProjectileFiringType.Burst:
                         FindTarget();
-                        firingAngle = 90f;
+                        baseAngle = 90f;
                         break;
                     case ProjectileFiringType.Spread:
-                        firingAngle = 90 + weaponData.amountAngle * i;
+                        baseAngle = 90 + weaponData.amountAngle * i;
                         break;
                     case ProjectileFiringType.AlternatingSpread:
                         int sign = (i % 2 == 0) ? 1 : -1;
                         int magnitude = (i + 1) / 2;
-                        firingAngle = 90 + weaponData.amountAngle * sign * magnitude;
+                        baseAngle = 90 + weaponData.amountAngle * sign * magnitude;
                         break;
                     case ProjectileFiringType.Omnidirectional:
-                        firingAngle = 90 + i * 360 / stats.amount;
+                        baseAngle = 90 + i * 360 / stats.amount;
                         break;
                 }
+
                 for (int k = 0; k < weaponData.spreadAmount + 1; k++)
                 {
+                    float angle = baseAngle;
+
                     switch (weaponData.projectileSpreadType)
                     {
                         case ProjectileFiringType.Spread:
-                            firingAngle += weaponData.amountAngle * i;
+                            angle += weaponData.spreadAngle * k;
                             break;
                         case ProjectileFiringType.AlternatingSpread:
-                            int sign = (i % 2 == 0) ? 1 : -1;
-                            int magnitude = (i + 1) / 2;
-                            firingAngle += weaponData.amountAngle * sign * magnitude;
+                            int sign = (k % 2 == 0) ? 1 : -1;
+                            int magnitude = (k + 1) / 2;
+                            angle += weaponData.spreadAngle * sign * magnitude;
                             break;
                         case ProjectileFiringType.Omnidirectional:
-                            firingAngle += i * 360 / stats.amount;
+                            angle += k * 360 / (weaponData.spreadAmount + 1);
                             break;
                         default:
                             if (k > 0)
                                 Debug.LogError("Spread type set improperly");
                             break;
                     }
-                    Vector3 direction = target == null ? new Vector2(0, 0) : target.transform.position - transform.position;
+
+                    Vector3 direction = target == null ? Vector2.zero : target.transform.position - transform.position;
 
                     GameObject proj = ProjectileManager.Instance.InstantiateProjectile(
                         weaponData.projectile,
                         transform.position,
-                        Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - firingAngle),
+                        Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - angle),
                         target == null ? null : target.gameObject,
                         gameObject.GetComponent<CharacterController>(),
                         stats,
                         gameObject.layer == 6,
                         weaponData.statusConditions,
                         weaponData.element
-                        );
+                    );
                     spawnedObjects.Add(proj);
                 }
+
                 yield return new WaitForSeconds(weaponData.amountDelay);
             }
         }
