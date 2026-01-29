532cf9fc3d1a177a06e476a9330b0e1fd639acd7
MonkeSandwich <milosstrugger@gmail.com>
2026-01-29 12:21:25 -0500

    menu manager fixes

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,51 +1,57 @@
 using UnityEngine;
 using DG.Tweening;
-using System.Collections.Generic;
 
 public enum UIDirection { Left, Right, Top, Bottom }
 
 public class MenuManager : MonoBehaviour
 {
-    public float duration = 0.5f;
-    public float transitionDelay = 1.0f;
-    public RectTransform title, startMenu, loadingScreen;
+    [Header("Animation Settings")]
+    public float enterDuration = 2f;
+    public float exitDuration = 1f;
+    public Ease easeIn = Ease.InQuint;
+    public Ease easeOut = Ease.OutQuint;
 
-    public void SwitchMenus(List<RectTransform> toExit, List<RectTransform> toEnter, UIDirection exitDir, UIDirection enterDir)
-    {
-        Sequence s = DOTween.Sequence();
+    [Header("Menu References")]
+    public RectTransform title;
+    public RectTransform startMenu;
+    public RectTransform loadingScreen;
 
-        foreach (var menu in toExit)
-        {
-            s.Join(Exit(menu, exitDir));
-        }
-
-        s.AppendInterval(transitionDelay);
-
-        foreach (var menu in toEnter)
-        {
-            s.Join(Enter(menu, enterDir));
-        }
-    }
+    [Header("Position Offsets")]
+    public Vector2 screenOffset = new Vector2(320, 180);
 
     public void Start()
     {
-        List<RectTransform> outMenus = new List<RectTransform> { title, startMenu };
-        List<RectTransform> inMenus = new List<RectTransform> { loadingScreen };
-
-        SwitchMenus(outMenus, inMenus, UIDirection.Top, UIDirection.Bottom);
+        Sequence s = DOTween.Sequence();
+        s.Append(Exit(loadingScreen, UIDirection.Top));
+        s.Append(Enter(title, UIDirection.Top));
+        s.Append(Enter(startMenu, UIDirection.Left));
     }
+
+
+    public void OpenFromBottom(RectTransform menu) => Enter(menu, UIDirection.Bottom);
+    public void OpenFromTop(RectTransform menu) => Enter(menu, UIDirection.Top);
+    public void OpenFromLeft(RectTransform menu) => Enter(menu, UIDirection.Left);
+    public void OpenFromRight(RectTransform menu) => Enter(menu, UIDirection.Right);
+
+    public void ExitToBottom(RectTransform menu) => Exit(menu, UIDirection.Bottom);
+    public void ExitToTop(RectTransform menu) => Exit(menu, UIDirection.Top);
+    public void ExitToLeft(RectTransform menu) => Exit(menu, UIDirection.Left);
+    public void ExitToRight(RectTransform menu) => Exit(menu, UIDirection.Right);
+
 
     public Tween Enter(RectTransform menu, UIDirection from)
     {
-        menu.anchoredPosition = GetPosForDirection(from);
+        if (menu == null) return null;
         menu.gameObject.SetActive(true);
-        return menu.DOAnchorPos(Vector2.zero, duration).SetEase(Ease.OutQuint);
+        menu.anchoredPosition = GetPosForDirection(from);
+        return menu.DOAnchorPos(Vector2.zero, enterDuration).SetEase(easeOut);
     }
 
     public Tween Exit(RectTransform menu, UIDirection to)
     {
-        return menu.DOAnchorPos(GetPosForDirection(to), duration)
-            .SetEase(Ease.InQuint)
+        if (menu == null) return null;
+        return menu.DOAnchorPos(GetPosForDirection(to), exitDuration)
+            .SetEase(easeIn)
             .OnComplete(() => menu.gameObject.SetActive(false));
     }
 
@@ -53,10 +59,10 @@ public class MenuManager : MonoBehaviour
     {
         return dir switch
         {
-            UIDirection.Left => new Vector2(-320, 0),
-            UIDirection.Right => new Vector2(320, 0),
-            UIDirection.Top => new Vector2(0, 180),
-            UIDirection.Bottom => new Vector2(0, -180),
+            UIDirection.Left => new Vector2(-screenOffset.x, 0),
+            UIDirection.Right => new Vector2(screenOffset.x, 0),
+            UIDirection.Top => new Vector2(0, screenOffset.y),
+            UIDirection.Bottom => new Vector2(0, -screenOffset.y),
             _ => Vector2.zero
         };
     }
