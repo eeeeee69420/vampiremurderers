cd4bde371d23639a9ef5c2df5ab635fd9e00d31f
MonkeSandwich <milosstrugger@gmail.com>
2026-02-14 16:55:18 -0500

    enemy manager

---
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager Instance;
+
+    public GameObject universalEnemyPrefab;
+
+    public List<GameObject> enemies;
+    public List<GameObject> disabledEnemies;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+    public GameObject InstantiateEnemy(EnemyData enemyData, Vector3 position, Transform parent = null)
+    {
+        GameObject enemy;
+        if (disabledEnemies.Count == 0)
+        {
+            enemy = Instantiate(universalEnemyPrefab);
+            enemies.Add(enemy);
+        }
+        else
+        {
+            enemy = disabledEnemies[0];
+            disabledEnemies.RemoveAt(0);
+        }
+        if (parent != null)
+            enemy.transform.SetParent(parent.transform, false);
+        else
+            enemy.transform.SetParent(null);
+
+        enemy.transform.SetPositionAndRotation(position, Quaternion.identity);
+        enemy.transform.localScale = Vector3.one;
+
+        EnemySetup enemySetup = enemy.GetComponent<EnemySetup>();
+        enemySetup.enemyData = enemyData;
+        enemySetup.Initialize();
+        enemy.SetActive(true);
+        return enemy;
+    }
+    public void DisableEnemy(GameObject enemy)
+    {
+        enemy.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        enemy.transform.localScale = Vector3.one;
+        enemy.SetActive(false);
+        foreach (var weapon in enemy.GetComponentsInChildren<Weapon>())
+        { Destroy(weapon); }
+        disabledEnemies.Add(enemy);
+    }
+}
